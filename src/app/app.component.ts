f1debbd1256c8da9a1125bd0f99cd8218fcf23c8
Didier Domínguez <dadu0699@hotmail.com>
2019-09-22 22:33:34 -0600

    [UPDATE] Code refactoring
    Code refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['../assets/css/login.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Mascotas';
